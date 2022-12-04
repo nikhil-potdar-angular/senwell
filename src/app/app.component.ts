cbe32a2559ad0945ce3cbfaa4811c8609f8cabba
nikhilp-angularminds <nikhil@angularminds.in>
2022-12-04 14:54:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pagination';
+export class AppComponent implements OnInit {
+  title = 'app';
+  names: any[] = ["Megan", "Tom", "Mellisa", "Tom", "John", "Megan", "Megan", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Megan", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Megan", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Sam", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Megan", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Megan", "Tom", "Mellisa", "Tom", "John", "Megan",
+    "Megan", "Tom", "Mellisa", "Tom", "John", "Megan"
+  ];
+  finalArr:any=[]
+ 
+  constructor() { }
+
+  ngOnInit() {
+
+    const count = this.names.reduce((accumulator, value) => {
+      return { ...accumulator, [value]: (accumulator[value] || 0) + 1 };
+    }, {});
+
+    console.log(count)
+    this.finalArr = Object.entries(count);
+
+  }
 }
